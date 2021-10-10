0e6480529fc6e6c7b597c0eae4d6107bd696970c
sagar shahi <shahisarkar05@gmail.com>
2021-10-09 22:18:58 -0400

    changes something

---
+++ Apple.java
@@ -0,0 +1,11 @@
+package first.java;
+
+public class Apple {
+
+	public static void main(String[] args) {
+		System.out.println("These apples are very big");
+                System.out.println("These apple are very good")    
+
+	}
+
+}
---
+++ Mango.java
@@ -0,0 +1,11 @@
+
+package first.java;
+
+public class Mango {
+
+	public static void main(String[] args) {
+		System.out.println("These mangoes very very good");
+
+	}s
+
+}
